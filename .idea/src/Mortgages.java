dd87d463bdd23ecf3a0e5e26f93eecf3913fd1e3
ryanpleach <46663647+ryanpleach@users.noreply.github.com>
2019-10-02 17:54:19 -0500

    try to commit

---
+++ .idea/src/Mortgages.java
@@ -0,0 +1,12 @@
+public class Mortgages {
+
+    public static void main(String[] args) {
+        System.out.println(calculateTotalPayment(1001,0,0));
+    }
+
+    public static int calculateTotalPayment(int initialDebt, int interest, int repayment) {
+        int initialDeposit = initialDebt / 10;
+        System.out.println("initial deposit made is " + initialDeposit);
+        return 0;
+    }
+}
